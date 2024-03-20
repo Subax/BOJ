3d841721d979890545254ec8da7ac1987615a575
Subax <chb3327@gmail.com>
2024-03-20 15:03:57 +0900

    boj_15552

---
+++ BOJ/15552.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "hello world!\n";
+    cin.tie(NULL); // cin과 cout의 묶음을 풀어줌
+    ios::sync_with_stdio(false);
+}
